ba35d8298582c57ed655874a8191155ae6fd62ed
Guesarnya <mr.danil85@gmail.com>
2025-07-29 22:42:05 +0300

    update

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://localhost:3000/chat/message', {
+        const response = await fetch('https://sexchat.onrender.com/chat/message', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
